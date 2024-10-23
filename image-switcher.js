6b39b12605993dc83316afb830343aeb36692eac
Emonora <114265017+Emonora@users.noreply.github.com>
2024-10-23 09:23:56 -0500

    added more images to the image switcher

--- image-switcher.js
+++ image-switcher.js
@@ -2,6 +2,9 @@ const images = [
     'assets/globalgoal.svg',
     'assets/posteer1.jpg',
     'assets/posteer2.jpg',
+    'assets/posteer3.png',
+    'assets/posteer4.png',
+    'assets/posteer5.png'
 ];
 
 let currentIndex = 0;
