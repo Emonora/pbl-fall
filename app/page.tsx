540c2a72be41bfcab2f59521714ca38bef873a74
Emonora <114265017+Emonora@users.noreply.github.com>
2024-10-27 15:47:36 -0500

    added styling to the amendment page

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
   return (
     <>
     <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="mx-auto text-center text-3xl">Promoting Mental Health</h1>
+      <h1 className="text-3xl">Promoting Mental Health</h1>
       <p className="text-center text-sm mt-4 w-1/4 t-40 italic">This is our pbl, created by august, ryan, and con. You can find any necessary information about our project on the sub pages of this website.</p>
       <div className="absolute bottom-0 left-0 p-4 text-white">
-        <p>Made with Next.js</p>
+        <p className="text-sm">Made with Next.js, and love from <a href="https://github.com/Emonora">Emonora</a></p>
         <Image
           src="/next.svg"
           alt="Next.js logo"
-          width={100}
-          height={40}
+          width={25}
+          height={10}
         />
       </div>
       <Link href="/propo">
--- app/propo/page.tsx
+++ app/propo/page.tsx
@@ -1,5 +1,18 @@
+import Image from 'next/image'
+
 export default function Home () {
     return (
-        <h1>Amendment Proposal</h1>
+        <main className='flex flex-col items-center justify-center h-screen'>
+            <h1 className='text-3xl'>Amendment Proposal</h1>
+            <div className="absolute bottom-0 left-0 p-4 text-white">
+                <p className="text-sm">Made with Next.js, and love from <a href="https://github.com/Emonora">Emonora</a></p>
+                <Image
+                    src="/next.svg"
+                    alt="Next.js logo"
+                    width={25}
+                    height={10}
+                />
+        </div>
+        </main>
     );
 }
