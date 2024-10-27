c37dd7aa61160130dcc8bf5000d1d4d7c8c6ecbb
Emonora <114265017+Emonora@users.noreply.github.com>
2024-10-27 15:25:27 -0500

    minor update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
+
 export default function Home() {
   return (
+    <>
     <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="mx-auto text-center text-3xl">Promoting Mental Health</h1>
       <p className="text-center text-sm mt-4 w-1/4 t-40 italic">This is our pbl, created by august, ryan, and con. You can find any necessary information about our project on the sub pages of this website.</p>
@@ -20,5 +22,6 @@ export default function Home() {
       </button>
       </Link>
     </main>
+    </>
   );
 }
