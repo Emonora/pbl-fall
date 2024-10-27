36393a679de8f48a275434655a9457a08971d9a2
Emonora <114265017+Emonora@users.noreply.github.com>
2024-10-27 13:23:12 -0500

    update to the website

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="mx-auto text-center text-3xl">Promoting Mental Health</h1>
+      <p className="text-center text-sm mt-4 w-1/4 t-40 italic">This is our pbl, created by august, ryan, and con. You can find any necessary information about our project on the sub pages of this website.</p>
+      <div className="absolute bottom-0 left-0 p-4 text-white">
+        <p>Made with Next.js</p>
+        <Image
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={40}
+        />
+      </div>
+      <Link href="/propo">
+      <button className="menu__button absolute bottom-0 left-50 p-4 text-white">
+        <span>Amendment Proposal</span>
+      </button>
+      </Link>
+    </main>
+  );
+}
---
+++ app/propo/page.tsx
@@ -0,0 +1,5 @@
+export default function Home () {
+    return (
+        <h1>Amendment Proposal</h1>
+    );
+}
