578e3f992c6db6467d3353a5186e4f678637678a
Emonora <114265017+Emonora@users.noreply.github.com>
2024-10-28 08:19:26 -0500

    minor update

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
     '/poster3.png'
   ];
   return (
-    <>
     <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl">Promoting Mental Health</h1>
       <p className="text-center text-sm mt-4 w-1/4 t-40 italic">This is our pbl, created by august, ryan, and con. You can find any necessary information about our project on the sub pages of this website.</p>
@@ -25,13 +24,11 @@ export default function Home() {
 
       <ImageSwitcher images={images} />
 
-
       <Link href="/propo">
         <button className="menu__button absolute bottom-0 left-50 p-4 text-white">
           <span>Amendment Proposal</span>
         </button>
       </Link>
     </main>
-    </>
   );
 }
