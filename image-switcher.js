62d174a5cf79791dc5ac8dd0c9d50f68fd482ef6
Emonora <114265017+Emonora@users.noreply.github.com>
2024-10-31 12:49:38 -0500

    added jerry

--- image-switcher.js
+++ image-switcher.js
@@ -7,11 +7,18 @@ const images = [
     'assets/posteer5.png'
 ];
 
+const image = 'mmmmmm.jpg'
+
 let currentIndex = 0;
+let i = 0;
 
 function nextImage() {
+    if (i === 100000) {
+        document.getElementById('maskedImage').src = image;
+    }
     currentIndex = (currentIndex + 1) % images.length;
     document.getElementById('maskedImage').src = images[currentIndex];
+    i++
 }
 
 setInterval(nextImage, 700)
